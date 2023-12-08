c787bf6b91127fc29f19e3be9e59f4d27e3c6a72
gumrahsindar <gsindar@gmail.com>
2023-12-08 21:52:52 +0300

    add main card component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,46 @@
+import Form from './components/Form'
+import Card from './components/Card'
+
+import { useState } from 'react'
+
 import './App.css'
 
+export type Card = {
+  name: string
+  number: string | number
+  validDate: {
+    month: string
+    year: string
+  }
+  cvc: string
+}
+
 function App() {
+  const [card, setCard] = useState<Card>({
+    name: 'Jane Appleseed',
+    number: '0000 000 000 0000',
+    validDate: {
+      month: '00',
+      year: '00',
+    },
+    cvc: '123',
+  })
+
+  const handleInputChange = (field: string, value: string) => {
+    setCard({
+      ...card,
+      [field]: value,
+      validDate: {
+        ...card.validDate,
+        [field]: value,
+      },
+    })
+  }
+
   return (
     <main className='main-container'>
-      <section id='card' className='card-container'>
-        <div className='card card-back'>
-          <img
-            className='card-img'
-            src='/images/bg-card-back.png'
-            alt='card back side'
-          />
-          <div className='card-back-content'>
-            <p>000</p>
-          </div>
-        </div>
-        <div className='card card-front'>
-          <img
-            className='card-img'
-            src='/images/bg-card-front.png'
-            alt='card front side'
-          />
-          <div className='card-front-content'>
-            <img src='/images/card-logo.svg' alt='card logo' />
-            <p className='card-number'>0000 000 000 000</p>
-            <div className='card-footer'>
-              <p className='owner-name'>jane appleseed</p>
-              <p className='valid-date'>00/00</p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section id='form' className='form-container'>
-        <form action='' className='form'>
-          <div className='form-item'>
-            <label htmlFor='card-number'>Cardholder Name</label>
-            <input
-              type='text'
-              name='card-number'
-              id='card-number'
-              placeholder='e.g. Jane Appleseed'
-            />
-          </div>
-          <div className='form-item'>
-            <label htmlFor='card-number'>Card Number</label>
-            <input type='text' name='card-number' id='card-number' />
-          </div>
-
-          <div className='form-item exp-date-cvc'>
-            <div className='exp-date exp-date-mm'>
-              <label htmlFor='exp-date'>Exp. Date (mm yy)</label>
-              <input type='text' name='exp-date' id='exp-date' />
-            </div>
-            <div className='exp-date exp-date-yy'>
-              <label htmlFor='exp-date'>x</label>
-              <input type='text' name='exp-date' id='exp-date' />
-            </div>
-            <div className='cvc'>
-              <label htmlFor='cvc'>CVC</label>
-              <input type='text' name='cvc' id='cvc' />
-            </div>
-          </div>
-
-          <button type='submit'>Submit</button>
-        </form>
-      </section>
+      <Card card={card} />
+      <Form onInputChange={handleInputChange} />
     </main>
   )
 }
